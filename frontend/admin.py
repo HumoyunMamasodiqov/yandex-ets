72ff6e093ab1c648c8cd7ade6e44eaf789666bff
Humoyun Mamasodiqov <mushtariy3748@gmail.com>
2025-12-10 12:33:47 +0500

    yandex ets

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Mahsulotlar
 # Register your models here.
+
+
+@admin.register(Mahsulotlar)
+class MahsulotlarAdmin(admin.ModelAdmin):
+    list_display = ['brenname', 'yetkazishvaqti', 'bepulyetkazish', 'chegirmabilan']
+    search_fields = ['brenname', 'yetkazishvaqti', 'bepulyetkazish', 'chegirmabilan']
+python manage.py migrate
+python manage.py makemigrations 
--- frontend/models.py
+++ frontend/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Mahsulotlar(models.Model):
+    asosiyimg = models.ImageField(upload_to='asosiyimg/', blank=True, null=True)
+    brenname = models.CharField(max_length=20)
+    yetkazishvaqti = models.CharField(max_length=20)
+    bepulyetkazish = models.BooleanField(default=False)
+    chegirmabilan = models.CharField(max_length=20 , )
+
+    def __str__(self):
+        return self.brenname
